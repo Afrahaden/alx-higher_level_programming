4ae922c04aa7343b7cc7f81a42363978b90cbbe5
Afrah Aden <afrahaden05@gmail.com>
2022-11-02 13:17:23 -0700

    SQL - queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,10 @@
--- Script that creates the MySQL server user 'user_0d_1'
--- command to create user if not exists already
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_psw';
+-- A script that creates the MySQL server user user_0d_1
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- flush privileges after creating a user or granting a user permissions
+FLUSH PRIVILEGES;
+
+-- granting a user all permissions in root
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

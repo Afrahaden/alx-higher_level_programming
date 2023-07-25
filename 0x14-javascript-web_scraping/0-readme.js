b51ed19c2e9c8a25eb1b902f3b28d969a558e35b
Afrah Aden <afrahaden05@gmail.com>
2023-07-25 15:57:35 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const file = process.argv[2];
-
-fs.readFile(file, 'utf-8', function (error, contents) {
-	  if ((error)) { console.log(error); } else { process.stdout.write(contents); }
+fs.readFile(process.argv[2], 'utf-8', function (error, content) {
+	  console.log(error || content);
 });
